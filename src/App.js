fed69ed8954a394b7ea9aa11ea02c3889476b890
artem_g <artemis.gorb.14@gmail.com>
2022-05-19 18:00:29 +0300

    token check

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const apiUrl = "https://go-course-work.herokuapp.com/"
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(
-      localStorage.getItem('token').length > 0
+      localStorage.getItem('token') !== undefined
   );
   const path = isLoggedIn ? '/edit' : '/login'
   const [darkTheme, setDarkTheme] = useState(
